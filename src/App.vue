e3fb54ec432ccacb587faeeb65187711bf00ba81
Iverson <iversonwang40@gmail.com>
2023-10-07 17:53:40 +0800

    1. vite 專案 2. 套件: vue-router + element-plus

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <router-link class="m-5" to="/">Main </router-link>
+    <router-link to="/demoElTable">DemoElTable</router-link>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+  <router-view></router-view>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from 'vue-router';
+const routes = [
+  {
+    path:'/',
+    name: 'Main',
+    component: () => import('@/views/Main.vue'),
+  },
+  {
+    path:'/demoElTable',
+    name: 'DemoElTable',
+    component: () => import('@/views/DemoElTable.vue'),
+  }
+]
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+export default router;
