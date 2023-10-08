2b6b16a6bcb0e41122f2cfbecf31dd748801b93d
Iverson <iversonwang40@gmail.com>
2023-10-09 00:27:21 +0800

    1. 跨頁勾選 2. 換頁時，已被勾選的電影，會預先打勾

--- src/views/ElTable.vue
+++ src/views/ElTable.vue
@@ -1,6 +1,14 @@
 <template>
   <el-button @click="handleCurrentChange(1)">查電影</el-button>
-  <el-table :data="movieArray" style="width: 100%">
+  <el-button @click="logFinalCheckedMovies">目前勾選的電影</el-button>
+
+  <el-table
+    :data="movieArray"
+    style="width: 100%"
+    ref="tableRef"
+    @select="handleSelect"
+    @select-all="handleSelectAll"
+  >
     <el-table-column type="selection" width="55" />
     <el-table-column type="index" width="50" />
     <el-table-column prop="original_title" label="title" width="180" />
@@ -18,20 +26,22 @@
   </el-pagination>
 </template>
 
-<script setup>
-import { ref, reactive } from "vue";
+<script lang="ts" setup>
+import { ref, nextTick, watch } from "vue";
 import axios from "axios";
+import { ElTable } from "element-plus";
 
 const BASE_URL = "https://api.themoviedb.org/3/";
 const isLoading = ref(false);
-const movieArray = ref([]);
+const movieArray = ref<any[]>([]);
 const currentPage = ref(1);
 const pageCount = ref(0);
 
+// 查詢電影
 const filterMovies = async (page) => {
   isLoading.value = true;
   // console.log({ page, year, genreId });
-  console.log(import.meta.env);
+  // console.log(import.meta.env);
   return axios.get(`${BASE_URL}discover/movie`, {
     params: {
       api_key: import.meta.env.VITE_APP_apiKey,
@@ -44,11 +54,12 @@ const filterMovies = async (page) => {
   });
 };
 
+// 點分頁
 const handleCurrentChange = async (val) => {
   currentPage.value = val;
   try {
     const { data } = await filterMovies(val);
-    console.log("data", data);
+    // console.log("data", data);
     movieArray.value = data.results;
     pageCount.value = data.total_pages;
     isLoading.value = false;
@@ -58,5 +69,62 @@ const handleCurrentChange = async (val) => {
     movieArray.value = [];
     isLoading.value = false;
   }
+};
+
+// 此陣列紀錄一共勾選了哪些電影
+const finalCheckedMovies = ref<any[]>([]);
+const handleSelect = (selectedRows, selectedRow) => {
+  // 勾選時，finalCheckedMovies 加入此電影
+
+  // 此選項是否已經勾選
+  finalCheckedMovies.value?.find(({ id }) => id === selectedRow.id)
+    ? // 此選項已經勾選: 取消勾選時，finalCheckedMovies 移掉此電影
+      (finalCheckedMovies.value = finalCheckedMovies.value?.filter(
+        ({ id }) => id !== selectedRow.id
+      ))
+    : // 此選項尚未勾選: 勾選時，finalCheckedMovies 加入此電影
+      finalCheckedMovies.value?.push(selectedRow);
+};
+const handleSelectAll = (selectedRows) => {
+  // selectedRows 為按下全選鈕後，是全選/全取消
+
+  // 全取消
+  if (selectedRows.length === 0) {
+    // finalCheckedMovies 移走畫面上這些電影
+    finalCheckedMovies.value = finalCheckedMovies.value?.filter(
+      (finalCheckedMovie) =>
+        !movieArray.value?.find(({ id }) => id === finalCheckedMovie.id)
+    );
+    return;
+  }
+
+  // 全選
+  selectedRows.forEach((row) => {
+    // 若已勾選，則不做事
+    if (finalCheckedMovies.value?.find(({ id }) => id === row.id)) return;
+    // 若尚未勾選，則 finalCheckedMovies 加入此電影
+    finalCheckedMovies.value?.push(row);
+  });
+};
+
+// 換頁時，已被勾選的電影(已在 finalCheckedMovies 的電影)，會預先打勾
+const tableRef = ref<InstanceType<typeof ElTable>>();
+const restoreCheckedMovies = () => {
+  movieArray.value.forEach((movie) => {
+    tableRef.value?.toggleRowSelection(
+      movie,
+      !!finalCheckedMovies.value?.some(({ id }) => id === movie.id)
+    );
+  });
+};
+watch(movieArray, async (newVal) => {
+  await nextTick();
+  restoreCheckedMovies();
+});
+
+const logFinalCheckedMovies = () => {
+  finalCheckedMovies.value.forEach(({ original_title }) => {
+    console.log(original_title);
+  });
 };
 </script>
