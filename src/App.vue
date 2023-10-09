590c13a547076e48e3edd165faf9a45f73fa0cd9
Iverson <iversonwang40@gmail.com>
2023-10-09 18:28:34 +0800

    router 們拆出來一個 component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,7 @@
 <template>
   <LanguageOptions />
-  <div>
-    <router-link
-      v-for="{ to, heading } in routerArray"
-      :to="to"
-      class="mr-10px"
-      >{{ heading }}</router-link
-    >
-  </div>
+  <RouterLinks />
+
   <HelloWorld msg="Vite + Vue" />
   <router-view></router-view>
 </template>
@@ -15,37 +9,7 @@
 <script setup>
 import HelloWorld from "./components/HelloWorld.vue";
 import LanguageOptions from "./components/LanguageOptions.vue";
-
-const routerArray = [
-  {
-    to: "/",
-    heading: "Main",
-  },
-  {
-    to: "elTable",
-    heading: "elTable",
-  },
-  {
-    to: "slot",
-    heading: "Slot",
-  },
-  {
-    to: "named-slots",
-    heading: "Named Slots",
-  },
-  {
-    to: "defineProperty",
-    heading: "defineProperty",
-  },
-  {
-    to: "reactivity",
-    heading: "Reactivity",
-  },
-];
+import RouterLinks from "./components/RouterLinks.vue";
 </script>
 
-<style scoped>
-.mr-10px {
-  margin-right: 10px;
-}
-</style>
+<style scoped></style>
---
+++ src/components/RouterLinks.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <router-link
+      v-for="{ to, heading } in routerArray"
+      :to="to"
+      class="mr-10px"
+      >{{ heading }}</router-link
+    >
+  </div>
+</template>
+
+<script setup>
+const routerArray = [
+  {
+    to: "/",
+    heading: "Main",
+  },
+  {
+    to: "elTable",
+    heading: "elTable",
+  },
+  {
+    to: "slot",
+    heading: "Slot",
+  },
+  {
+    to: "named-slots",
+    heading: "Named Slots",
+  },
+  {
+    to: "defineProperty",
+    heading: "defineProperty",
+  },
+  {
+    to: "reactivity",
+    heading: "Reactivity",
+  },
+];
+</script>
+
+<style scoped>
+.mr-10px {
+  margin-right: 10px;
+}
+</style>
